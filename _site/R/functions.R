f5bbef027755ec5039d3362ac81ce4a36282965a
mcnakhaee <mcnakhaee@gmail.org>
2021-05-30 13:31:15 +0200

    static page

--- _site/R/functions.R
+++ _site/R/functions.R
@@ -18,3 +18,34 @@ create_proj_card = function(img_src, url, title, text, text2){
 
 
 
+create_mcnakhaee_post <- function(..., open = TRUE) {
+  
+  tmp <- distill::create_post(..., edit = FALSE)
+  
+  yaml <- readLines(tmp, n = 12)
+  
+  con <- file(tmp, open = "w")
+  
+  on.exit(close(con), add = TRUE)
+  
+  body <- 
+    '
+  
+#```{r setup, echo = TRUE, results = "hide", warning = FALSE, message = FALSE}
+knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE)
+
+library(tidyverse)
+
+theme_set(theme_ee())
+
+#```
+
+A message
+'
+
+xfun::write_utf8(yaml, con)
+xfun::write_utf8(body, con)
+
+if (open == TRUE) usethis::edit_file(tmp)
+
+}
